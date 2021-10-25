ef2ddf66466e1b4918aa8623b38047dbe6d47f14
Václav Thám <venca275tham@gmail.com>
2021-10-25 16:11:01 +0200

    Úvod pro piškvorky

---
+++ du01_vaclav_tham.py
@@ -0,0 +1,8 @@
+from turtle import circle, forward, xcor
+x=3
+y=3
+print("Ahoj právě si otevřel piškvorky")
+j= input("Zadej jmeno prvniho hrace: ")
+k= input("Zadej jmeno druheho hrace: ")
+print ("V tomto duelu proti sobe nastoupí ", j ,"a ", k ,".")
+print("Tato hra piskvorek je v poli ",x,"x",y)
